110d5505530224be2bc62f03c0c7f1b0a5860510
JC Smiley <jcsmileyjr@gmail.com>
2018-03-12 22:23:54 -0500

    Build main question's page with Q & A elements
    - Provide routing with Angular ui-router for the html page
    - build UI elements needed for testing
    - Linked html page to the controller and data from a service.
    - initial CSS

---
+++ questions/questionsController.js
@@ -0,0 +1,15 @@
+myApp.controller('questionsController',  function($scope, TestData){
+    //record the current test name being taken
+    $scope.currentTest = "scienceChp1";
+    
+    //get test data from the TestData service to be displayed 
+    $scope.studyQuestions = TestData.getData("scienceChp1");
+    
+    //method to get student answer to a question saved to the question object it came from
+    
+    $scope.completed = function(index, answer){
+        TestData.saveAnswer($scope.currentTest, index, answer);
+    }
+    
+
+});//end of Controller

1364496143fb2272c41178ed1c064d5077e0e1d8
JC Smiley <jcsmileyjr@gmail.com>
2018-03-17 15:13:20 -0500

    Setup the random Encouragment  feature
    - Create a controller that links the encouragment quotes to the nav.
    - Use AngularJS interval method to randomly show different quotes in the
    nav

---
+++ nav functions/scoreService.js
@@ -0,0 +1,33 @@
+/*A service that use a counter to keep score and return that score. */
+myApp.factory('Score', [ function () {
+    
+    var score = {"count":0};
+    var answered = {"questionsAnswered":0};
+    
+    var positiveFeedBack = ["Good Job", "Besides chocolate, you are my favorite", "Awesome", "I love  you", "You can do it", "Get that A, Get that A", "You are the Best", "Super-smart, that's what you are", "Please Lord, help this child of mine", "If you need help, come get me", "I turned out liking you a lot more than I originally planned", "Good friends don't let you do stupid things...alone",];
+     
+     return {
+         
+         //add one to score.count when user gets correct answer
+         addScore: function(){
+             score.count+= 1;
+         },
+         
+         //add one to score.count when user gets correct answer
+         addQuestionsAnswered: function(){
+             answered.questionsAnswered+= 1;            
+         },
+         
+         //return the current score
+         getScore: function(){                
+               return (score.count/answered.questionsAnswered)*100;
+         }, 
+         
+         //return a random positive feedback
+         getPositiveFeedBack: function(){
+              return positiveFeedBack[Math.floor(Math.random() * positiveFeedBack.length)];
+         }
+     }/*End of main Return*/
+    
+    
+}]);
--- questions/questionsController.js
+++ questions/questionsController.js
@@ -1,20 +1,17 @@
 myApp.controller('questionsController',  function($scope, TestData){
+  
+    
     //record the current test name being taken
     $scope.currentTest = "scienceChp1";
     
     //get test data from the TestData service to be displayed 
     $scope.studyQuestions = TestData.getData("scienceChp1");
     
-    //method to get student answer to a question saved to the question object it came from
-    
+    //method to get student answer to a question saved to the question object it came from    
     $scope.completed = function(index, answer){
-        TestData.saveAnswer($scope.currentTest, index, answer);
-        console.table($scope.studyQuestions);
+        TestData.saveAnswer($scope.currentTest, index, answer)
     }
     
-    
-    $scope.reset = function(answer){
-        answer = "";
-    }
+
 
 });//end of Controller

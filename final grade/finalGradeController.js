6d998a24de4921850e2a041a44ce405aef2e9227
JC Smiley <jcsmileyjr@gmail.com>
2018-03-19 10:45:02 -0500

    Add panda imgs to app
    - On the final grade page, different images is shown based on the grade.

--- final grade/finalGradeController.js
+++ final grade/finalGradeController.js
@@ -1,30 +1,37 @@
 myApp.controller('finalGradeController',  function($scope, Score){
+    
+    //initialize the image that will be shown on the final grade page based on the grade
+    $scope.imageBasedOnGrade = "/css/small_cartoon_pandas.jpg"
 
-    //initialize the number of correct answers
+    //initialize the number of correct and incorrect answers
     $scope.correctAnswers = 0;
     $scope.incorrectAnswers = 0;
     
-    //update and display the final score
+    //update and display the final score on the final grade page
     $scope.finalScore = Score.getScore();
 
     
-    //update and display the number of correct answers
+    //update and display the number of correct answers on the final grade page
     $scope.correctAnswers = Score.getCorrectAnswer();
     
-    //update and display the number of incorrect answers
+    //update and display the number of incorrect answers on the final grade page
     $scope.incorrectAnswers = Score.getIncorrectAnswer();
     
-    //business logic that determinen grade
+    //business logic that determines the grade display on the final grade page
     if($scope.finalScore >= 90){
-        return $scope.gradeLetter = "A";
+        $scope.imageBasedOnGrade = "/css/small_cartoon_pandas.jpg"
+        $scope.gradeLetter = "A";
     }else if($scope.finalScore >= 80) {
-        return $scope.gradeLetter = "B";
+        $scope.imageBasedOnGrade = "/css/small_white_panda.jpg"
+        $scope.gradeLetter = "B";
     }else if ($scope.finalScore >= 70){
-        return $scope.gradeLetter = "C";
+        $scope.imageBasedOnGrade = "/css/white_cute_panda.jpg"
+        $scope.gradeLetter = "C";
     }else if ($scope.finalScore >= 60){
-        return $scope.gradeLetter = "D";
+        $scope.imageBasedOnGrade = "/css/cute_panda.jpg"
+        $scope.gradeLetter = "D";
     }else {
-        return $scope.gradeLetter = "F";
+         $scope.gradeLetter = "F";
     }    
 
 
